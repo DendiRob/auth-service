088189e2f01713b9fd5c7d5e8247df1dd71aeed1
DendiRob <dendiroblek@gmail.com>
2024-09-08 16:15:59 +0400

    connect to postgres

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const PORT = process.env.PORT || 4321;
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  await app.listen(PORT, () => {
+    console.log(`Server is running on port: ${PORT}`);
+  });
 }
 bootstrap();
