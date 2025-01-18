6564323fbc0f089977da1a9ae28d1e1565dd37d9
DendiRob <dendiroblek@gmail.com>
2025-01-18 18:42:14 +0400

    deleted cacheable

--- src/common/configs/cache-manager.config.ts
+++ src/common/configs/cache-manager.config.ts
@@ -1,6 +1,5 @@
 import KeyvRedis, { Keyv } from '@keyv/redis';
 import { CacheModuleOptions } from '@nestjs/cache-manager';
-import { CacheableMemory } from 'cacheable';
 
 //  TODO: объявить переменные. настроить авторизацию в бд, настроить конфиг и дефолтные значения по кешу
 export const cacheManagerConfig: CacheModuleOptions = {
@@ -8,11 +7,6 @@ export const cacheManagerConfig: CacheModuleOptions = {
   useFactory: async () => {
     return {
       stores: [
-        // High performance in-memory cache with LRU and TTL
-        new Keyv({
-          store: new CacheableMemory({ ttl: 60000, lruSize: 5000 }),
-        }),
-
         // Redis Store
         new Keyv({
           store: new KeyvRedis('redis://localhost:6379'),
