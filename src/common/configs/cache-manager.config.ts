248562cf78f8b79b240410c53be39287e20865b5
DendiRob <dendiroblek@gmail.com>
2025-01-19 14:09:06 +0400

    fixed redis configuration

--- src/common/configs/cache-manager.config.ts
+++ src/common/configs/cache-manager.config.ts
@@ -1,12 +1,17 @@
-import KeyvRedis from '@keyv/redis';
+import { createKeyv } from '@keyv/redis';
 import { CacheModuleOptions } from '@nestjs/cache-manager';
 
 //  TODO: объявить переменные. настроить авторизацию в бд, настроить конфиг и дефолтные значения по кешу
 export const cacheManagerConfig: CacheModuleOptions = {
   isGlobal: true,
   useFactory: async () => {
+    const redis = createKeyv('redis://localhost:6379', {
+      namespace: process.env.REDIS_NAMESPACE,
+      keyPrefixSeparator: ':',
+    });
+
     return {
-      stores: [new KeyvRedis('redis://localhost:6379')],
+      stores: [redis],
     };
   },
 };
